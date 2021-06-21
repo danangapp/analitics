ac1ebdc0b028d2e66a6af375e2c389843c3b2273
Danang Rusmanto <danang@roots.co.id>
2021-06-21 13:02:16 +0700

    update all

--- src/insights/Views.js
+++ src/insights/Views.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Bar  } from 'react-chartjs-2';
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
 import { Col, Row, Card, Image, Button, Container, ListGroup, Tooltip, OverlayTrigger, Form, Navbar, Nav, Badge } from '@themesberg/react-bootstrap';
-
+import axios from 'axios';
 
 const data = {
   labels: ['1', '2', '3', '4', '5', '6'],
@@ -32,49 +32,106 @@ const options = {
   },
 };
 
-const PieChart = () => (
-  <>
-    <div className='header'>
-      <h1 className='title'>Per Click</h1>      
-    </div>
-    
-    <Container>
-      <Row>
-        <Col xs={6} className="text-center">
-        	<Card border="light" className="bg-white shadow-sm mb-4">
-      			<Card.Body>
-      				<h5 className="mb-4">March</h5>
-		          	<Bar data={data} options={options} />
-      			</Card.Body>
-    		</Card>
-        </Col>
-        <Col xs={6} className="text-center">
-        	<Card border="light" className="bg-white shadow-sm mb-4">
-      			<Card.Body>
-      				<h5 className="mb-4">April</h5>
-		          	<Bar data={data} options={options} />
-      			</Card.Body>
-    		</Card>
-        </Col>
-        <Col xs={6} className="text-center">
-        	<Card border="light" className="bg-white shadow-sm mb-4">
-      			<Card.Body>
-      				<h5 className="mb-4">May</h5>
-		          	<Bar data={data} options={options} />
-      			</Card.Body>
-    		</Card>
-        </Col>
-        <Col xs={6} className="text-center">
-        	<Card border="light" className="bg-white shadow-sm mb-4">
-      			<Card.Body>
-      				<h5 className="mb-4">June</h5>
-		          	<Bar data={data} options={options} />
-      			</Card.Body>
-    		</Card>
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+const PieChart = () => {
+  const [march, setMarch] = useState([]);
+  const [april, setApril] = useState([]);
+  const [may, setMay] = useState([]);
+  const [june, setJune] = useState([]);
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/colours/views/march`)
+      .then(function (res) {
+        var arr = [], arr2 = [];
+        for (const a in res.data) {
+          arr.push(res.data[a].dates);
+          arr2.push(res.data[a].views);
+        }
+
+        data.labels = arr;
+        data.datasets[0].data = arr2;
+        setMarch(data);
+      });
+    axios.get(`${process.env.REACT_APP_BASE_URL}/colours/views/april`)
+      .then(function (res) {
+        var arr = [], arr2 = [];
+        for (const a in res.data) {
+          arr.push(res.data[a].dates);
+          arr2.push(res.data[a].views);
+        }
+
+        data.labels = arr;
+        data.datasets[0].data = arr2;
+        setApril(data);
+      });
+    axios.get(`${process.env.REACT_APP_BASE_URL}/colours/views/may`)
+      .then(function (res) {
+        var arr = [], arr2 = [];
+        for (const a in res.data) {
+          arr.push(res.data[a].dates);
+          arr2.push(res.data[a].views);
+        }
+
+        data.labels = arr;
+        data.datasets[0].data = arr2;
+        setMay(data);
+      });
+    axios.get(`${process.env.REACT_APP_BASE_URL}/colours/views/june`)
+      .then(function (res) {
+        var arr = [], arr2 = [];
+        for (const a in res.data) {
+          arr.push(res.data[a].dates);
+          arr2.push(res.data[a].views);
+        }
+
+        data.labels = arr;
+        data.datasets[0].data = arr2;
+        setJune(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className='header'>
+        <h1 className='title'>Per Click</h1>
+      </div>
+
+      <Container>
+        <Row>
+          <Col xs={6} className="text-center">
+            <Card border="light" className="bg-white shadow-sm mb-4">
+              <Card.Body>
+                <h5 className="mb-4">June</h5>
+                <Bar data={june} options={options} />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={6} className="text-center">
+            <Card border="light" className="bg-white shadow-sm mb-4">
+              <Card.Body>
+                <h5 className="mb-4">May</h5>
+                <Bar data={may} options={options} />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={6} className="text-center">
+            <Card border="light" className="bg-white shadow-sm mb-4">
+              <Card.Body>
+                <h5 className="mb-4">April</h5>
+                <Bar data={april} options={options} />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={6} className="text-center">
+            <Card border="light" className="bg-white shadow-sm mb-4">
+              <Card.Body>
+                <h5 className="mb-4">March</h5>
+                <Bar data={march} options={options} />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+};
 
 export default PieChart;
